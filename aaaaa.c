afe7e185edbde67373d759b151ce41624f59aa4e
robertaaz <robertaaz.s19@gmail.com>
2024-10-08 09:38:57 -0300

    Add files via upload

---
+++ aaaaa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int idadeGabi = 17;
+    int idadeVivi = 16;
+
+    if (idadeGabi > idadeVivi ){
+        printf("Gabi e mais velha que Vivi");
+    }
+    else printf ("Vivi e mais velha que Gabi");
+}
