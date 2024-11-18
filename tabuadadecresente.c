7c7af1fef4dbcd2e12115907c6cb633400a0465d
robertaaz <robertaaz.s19@gmail.com>
2024-11-18 08:33:21 -0300

    Add files via upload

---
+++ tabuadadecresente.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+int n, c, r;
+
+printf("Numero? ");
+scanf("%d", &n);
+printf("\nTabuada do %d\n\n", n);
+
+     for(c=10 ; c>=1 ; c--) {
+        r = n*c;
+        printf("%d x %2d = %3d\n", n, c, r);
+     }
+return 0;
+ }
