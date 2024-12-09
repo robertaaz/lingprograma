25c70ab9f1554a37b5aec66a2d41c879069900ba
robertaaz <robertaaz.s19@gmail.com>
2024-12-09 09:04:45 -0300

    Add files via upload

---
+++ mediaa.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    int a, i;
+    float n, m, soma=0;
+
+    printf("Quantos alunos sao?");
+    scanf("%d", &a);
+ 
+ for(i=1; i<=a; i++){
+    printf("%da. Digite a nota dos alunos:", i);
+    scanf("%f", &n);
+    soma = soma + n;
+ }
+ m = soma/a;
+
+ printf("a media dos alunos e: %.1f\n", m);
+    }
